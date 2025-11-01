0e838d2d1bdf93d6cd260cc55adfd4b4458dc453
DhanushReddyk999 <komatireddydhanushreddy@gmail.com>
2025-11-01 16:42:33 +0530

    added mailing

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const connectDB = require("./config/db");
 const authRoutes = require("./routes/authRoutes");
 const schemeRoutes = require("./routes/schemeRoutes");
-const reminderRoutes = require("./routes/eligibilityRoutes");
+const reminderRoutes = require("./routes/reminderRoutes");
 
 // Load environment variables
 dotenv.config();
@@ -22,7 +22,7 @@ app.use(express.urlencoded({ extended: true }));
 // Routes
 app.use("/api/users", authRoutes);
 app.use("/api/schemes", schemeRoutes);
-// app.use("/api/reminders", reminderRoutes);
+app.use("/api/reminders", reminderRoutes);
 
 
 const PORT = process.env.PORT || 5000;
